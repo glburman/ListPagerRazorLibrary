b13d9bb6cd4dd771df3ad29d1fbefe6af1117e95
glburman <gregburman@hotmail.com>
2020-01-23 21:58:51 -0700

    misc

---
+++ ListPagerRazorLibrary/AppConstants.cs
@@ -0,0 +1,43 @@
+﻿namespace ListPagerRazorLibrary
+{
+    public static class AppConstants
+    {
+#if DEBUG
+        public const bool IS_DEBUG = true;
+#else
+        public const bool IS_DEBUG = false;
+#endif
+        //Model constants
+        public const int PAGE_NUMBER = 1;
+        public const int PAGE_SIZE = 20;
+        public const int MAX_PAGE_LINKS = 5;
+        public const int PAGE_COUNT = 0;
+        public const int DROP_DOWN_INC = 5;
+        public const int MAX_PAGE_SIZE = 300;
+        public const int MIN_PAGE_SIZE = 1;
+        public const string PAGE_SIZE_INPUT = "pageSizeIn";
+        //View Constants
+        public const string VIEWNAME_ARROWS = "ListPagerArrows";
+        public const string VIEWNAME_DROPDOWN = "ListPagerDropdown";
+        public const string VIEWNAME_LINKS = "ListPagerLinks";
+        public const string VIEWNAME_PAGEOF = "ListPagerPageOf";
+        public const string VIEWNAME_PAGESIZE = "ListPagerPageSize";
+        public const string VIEWNAME_POSTFORM = "ListPagerPostForm";
+        public const string VIEWNAME_RECORDS = "ListPagerRecords";
+        public const string PAGE_LINK_ACTIVE_CLASS = "active";
+        public const string DROPDOWN_BOUNDARY_CLASS = "last";
+        public const string PAGE_LINK_FORMAT = "/?page={0}";
+        //Component Constants
+        public const string VIEW_PATH_LISTPAGER = "../ListPager";
+        public const string VIEW_PATH_LISTPAGER_ARROWS = "../ListPagerArrows";
+        public const string VIEW_PATH_LISTPAGER_DROPDOWN = "../ListPagerDropdown";
+        public const string VIEW_PATH_LISTPAGER_LINKS = "../ListPagerLinks";
+        public const string VIEW_PATH_LISTPAGER_PAGEOF = "../ListPagerPageOf";
+        public const string VIEW_PATH_LISTPAGER_PAGESIZE = "../ListPagerPageSize";
+        public const string VIEW_PATH_LISTPAGER_POSTFORM = "../ListPagerPostForm";
+        public const string VIEW_PATH_LISTPAGER_RECORDS = "../ListPagerRecords";
+        //Assembly Constants
+        public const string ASSEMBLY_TYPE_SCRIPT = "ListPagerRazorLibrary.js";
+        public const string ASSEMBLY_TYPE_STYLES = "ListPagerRazorLibrary.css";
+    }
+}
---
+++ ListPagerRazorLibrary/Models/ListPagerDropdownModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListPagerRazorLibrary.Models
+{
+    public class ListPagerDropdownModel
+    {
+        public ListPagerDropdownModel(ListPagerModel model)
+        {
+            Increments = new List<int>();
+            PageCount = model.PageCount;
+            for (int step = 1; step <= model.PageCount; step++)
+            {
+                if ((step % model.DropDownIncrement == 0 
+                        || step == 1 
+                        || step == PageCount
+                    ) 
+                    && (step < model.Start 
+                        || step > model.End))
+                {
+                    Increments.Add(step);
+                }
+            }
+        }
+        public List<int> Increments { get; set; }
+        public int PageCount { get; set; }
+    }
+}
