57724c9e5fec8f0f002f777a5803262e0cf36f0f
glburman <gregburman@hotmail.com>
2020-11-04 12:50:55 -0700

    Update Gulp & remove gulp-clean-css to eliminate vulnerabilities

--- ListPagerRazorLibrary/gulpfile.js
+++ ListPagerRazorLibrary/gulpfile.js
@@ -1,11 +1,6 @@
-/// <binding BeforeBuild='default' />
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-const del = require('del');
 const concat = require('gulp-concat');
-const cleanCss = require('gulp-clean-css');
-
-
 
 gulp.task('pager', function () {
     return gulp.src([
@@ -13,7 +8,6 @@ gulp.task('pager', function () {
         'wwwroot/scss/pager-theme-default.scss'])
         .pipe(sass())
         .pipe(concat('pager.css'))
-        .pipe(cleanCss({ level: { 2: { restructureRules: true } } }))
         .pipe(gulp.dest('wwwroot/css'))
 })
 
@@ -23,7 +17,6 @@ gulp.task('pager-dark', function () {
         'wwwroot/scss/pager-theme-dark.scss'])
         .pipe(sass())
         .pipe(concat('pager-dark.css'))
-        .pipe(cleanCss({ level: { 2: { restructureRules: true } } }))
         .pipe(gulp.dest('wwwroot/css'))
 })
 
@@ -33,7 +26,6 @@ gulp.task('pager-neon', function () {
         'wwwroot/scss/pager-theme-neon.scss'])
         .pipe(sass())
         .pipe(concat('pager-neon.css'))
-        .pipe(cleanCss({ level: { 2: { restructureRules: true } } }))
         .pipe(gulp.dest('wwwroot/css'))
 })
 
